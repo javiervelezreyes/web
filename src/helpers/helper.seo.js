1de19a4fb8d93f40e66472c7aa402b7f93ea5946
javier velez <javier.velez.reyes@gmail.com>
2024-05-26 21:37:19 +0200

    SEO mdata

--- src/helpers/helper.seo.js
+++ src/helpers/helper.seo.js
@@ -1,17 +1,18 @@
 const BR    = '\n'
 const NL    = ''
-const BS    = ' '
 const BD    = '  '
 const ANY   = '*'
 const ALL   = '[^/]*'
 const START = '^'
 const END   = '$'
+const SLASH = '/'
 
 function Helper () {
 
   function When (...paths) {
     return function (path) {
       return paths.map (function (path) {
+        path.endsWith (SLASH) && (path = path.slice (0, -1))
         return RegExp (START + path.split(ANY).join(ALL) + END)
       }).some (function (exp) {
         return exp.test (path)
